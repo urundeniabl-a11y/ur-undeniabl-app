bcb326ea0499807a87fdd4db05e475ad33f58be2
urundeniabl-a11y <urundeniabl@gmail.com>
2026-06-30 02:19:31 -0500

    Delete page.tsx

--- page.tsx
+++
@@ -1,9 +0,0 @@
-import { RecipeDashboard } from "@/components/citrus/recipe-dashboard"
-
-export default function Page() {
-  return (
-    <main className="min-h-screen bg-background">
-      <RecipeDashboard />
-    </main>
-  )
-}
