a2d6d6be67d7c5158fec8e80c68b9fc90708c28f
urundeniabl-a11y <urundeniabl@gmail.com>
2026-06-30 01:29:17 -0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,9 @@
+import { RecipeDashboard } from "@/components/citrus/recipe-dashboard"
+
+export default function Page() {
+  return (
+    <main className="min-h-screen bg-background">
+      <RecipeDashboard />
+    </main>
+  )
+}
